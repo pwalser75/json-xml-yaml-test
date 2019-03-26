d70735f1727f1e43415e3ee13e78c4bd1ddbd6ea
Peter Walser <peter.walser@ti8m.ch>
2019-03-26 11:50:45 +0100

    Refactored Movie to immutable with builder

--- src/main/java/ch/frostnova/test/jackson/test/util/domain/Movie.java
+++ src/main/java/ch/frostnova/test/jackson/test/util/domain/Movie.java
@@ -11,10 +11,14 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 
 /**
- * Test domain object
+ * Test domain object, immutable - using internal builder.
  *
  * @author pwalser
  * @since 25.01.2018.
@@ -63,92 +67,112 @@ public class Movie {
 
     public static Movie create() {
 
-        Movie movie = new Movie();
-        movie.setTitle("Blade Runner");
-        movie.setYear(1982);
-        movie.setGenres(Arrays.asList(Genre.SCI_FI, Genre.THRILLER));
-        movie.getRatings().put("IMDB", 8.2);
-        movie.getRatings().put("Metacritic", 89d);
-        movie.setSynopsis("A blade runner must pursue and terminate four replicants\n who stole a ship in space and have returned to Earth to find their creator.");
-        movie.setAspectRatio(new AspectRatio(2.39, 1));
-        movie.getActors().add(new Actor("Harrison", "Ford", LocalDate.of(1942, 7, 13)));
-        movie.getActors().add(new Actor("Rutger", "Hauer", LocalDate.of(1944, 1, 23)));
-        movie.getActors().add(new Actor("Sean", "Young", LocalDate.of(1959, 11, 20)));
-        movie.getMetadata().set("director", "Ridley Scott");
-        movie.getMetadata().set("screenplay", "Hampton Fancher, David Webb Peoples");
-        movie.getMetadata().set("release-date", "1982-06-25");
-        return movie;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String name) {
-        title = name;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public List<Genre> getGenres() {
-        return genres;
-    }
-
-    public void setGenres(List<Genre> genres) {
-        this.genres = genres;
-    }
-
-    public Map<String, Number> getRatings() {
-        return ratings;
-    }
-
-    public void setRatings(Map<String, Number> ratings) {
-        this.ratings = ratings;
+        return Movie.builder()
+                .title("Blade Runner")
+                .year(1982)
+                .addGenre(Genre.SCI_FI)
+                .addGenre(Genre.THRILLER)
+                .addRating("IMDB", 8.2)
+                .addRating("Metacritic", 89d)
+                .synopsis("A blade runner must pursue and terminate four replicants\n who stole a ship in space and have returned to Earth to find their creator.")
+                .aspectRatio(new AspectRatio(2.39, 1))
+                .addActor(new Actor("Harrison", "Ford", LocalDate.of(1942, 7, 13)))
+                .addActor(new Actor("Rutger", "Hauer", LocalDate.of(1944, 1, 23)))
+                .addActor(new Actor("Sean", "Young", LocalDate.of(1959, 11, 20)))
+                .addMetadata("director", "Ridley Scott")
+                .addMetadata("screenplay", "Hampton Fancher, David Webb Peoples")
+                .addMetadata("release-date", "1982-06-25")
+                .get();
     }
 
     public ZonedDateTime getCreated() {
         return created;
     }
 
-    public void setCreated(ZonedDateTime created) {
-        this.created = created;
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public Map<String, Number> getRatings() {
+        return ratings;
     }
 
     public String getSynopsis() {
         return synopsis;
     }
 
-    public void setSynopsis(String synopsis) {
-        this.synopsis = synopsis;
+    public List<Actor> getActors() {
+        return actors;
     }
 
     public AspectRatio getAspectRatio() {
         return aspectRatio;
     }
 
-    public void setAspectRatio(AspectRatio aspectRatio) {
-        this.aspectRatio = aspectRatio;
-    }
-
-    public List<Actor> getActors() {
-        return actors;
-    }
-
-    public void setActors(List<Actor> actors) {
-        this.actors = actors;
-    }
-
     public Metadata getMetadata() {
         return metadata;
     }
 
-    public void setMetadata(Metadata metadata) {
-        this.metadata = metadata;
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+
+        private final Movie instance = new Movie();
+        private boolean consumed;
+
+        private Builder set(Consumer<Movie> access) {
+            if (consumed) {
+                throw new IllegalStateException("already consumed");
+            }
+            access.accept(instance);
+            return this;
+        }
+
+        public Builder title(String title) {
+            return set(x -> x.title = title);
+        }
+
+        public Builder year(int year) {
+            return set(x -> x.year = year);
+        }
+
+        public Builder addGenre(Genre genre) {
+            return set(x -> x.getGenres().add(genre));
+        }
+
+        public Builder addRating(String source, Number rating) {
+            return set(x -> x.ratings.put(source, rating));
+        }
+
+        public Builder synopsis(String synopsis) {
+            return set(x -> x.synopsis = synopsis);
+        }
+
+        public Builder addActor(Actor actor) {
+            return set(x -> x.actors.add(actor));
+        }
+
+        public Builder aspectRatio(AspectRatio aspectRatio) {
+            return set(x -> x.aspectRatio = aspectRatio);
+        }
+
+        public Builder addMetadata(String key, String value) {
+            return set(x -> x.metadata.set(key, value));
+        }
+
+        public Movie get() {
+            consumed = true;
+            return instance;
+        }
     }
 }
