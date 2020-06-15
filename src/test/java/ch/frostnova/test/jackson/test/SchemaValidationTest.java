32567a11cbeb71dd1be864723bc4105cccf238d1
Peter Walser <pwalser@frostnova.ch>
2020-06-15 16:20:05 +0200

    enhanced schema validation

--- src/test/java/ch/frostnova/test/jackson/test/SchemaValidationTest.java
+++ src/test/java/ch/frostnova/test/jackson/test/SchemaValidationTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class SchemaValidationTest {
@@ -55,15 +56,18 @@ public class SchemaValidationTest {
             Set<String> validationMessages = ex.getAllMessages().stream().map(String::valueOf).collect(Collectors.toSet());
             assertTrue(validationMessages.contains("#/metadata/something: expected type: String, found: Boolean"));
             assertTrue(validationMessages.contains("#/aspect-ratio: string [555] does not match pattern ^\\d+(\\.\\d+)?\\:\\d+(\\.\\d+)?$"));
-            assertTrue(validationMessages.contains("#/year: -3.0 is not higher or equal to 0"));
+            assertTrue(validationMessages.contains("#/year: expected type: Integer, found: Double"));
             assertTrue(validationMessages.contains("#/genres/0: expected type: String, found: Integer"));
             assertTrue(validationMessages.contains("#/created: [yesterday] is not a valid date-time. Expected [yyyy-MM-dd'T'HH:mm:ssZ, yyyy-MM-dd'T'HH:mm:ss.[0-9]{1,9}Z]"));
             assertTrue(validationMessages.contains("#/ratings/IMDB: -8.2 is not higher or equal to 0"));
             assertTrue(validationMessages.contains("#: required key [title] not found"));
             assertTrue(validationMessages.contains("#: extraneous key [nobodyExpects] is not permitted"));
+            assertTrue(validationMessages.contains("#/actors/0/firstName: string [?] does not match pattern ^\\p{Lu}\\p{Ll}+$"));
             assertTrue(validationMessages.contains("#/actors/0/dateOfBirth: string [1942-42-13] does not match pattern \\d{4}-(0[1-9]|[1-2][0-9]|3[0-1])-(0[1-9]|[1-2][0-9]|3[0-1])"));
             assertTrue(validationMessages.contains("#/actors/0/age: -77.0 is not higher or equal to 0"));
             assertTrue(validationMessages.contains("#/actors/0: required key [lastName] not found"));
+            assertTrue(validationMessages.contains("#/actors/1/lastName: string [dREBIN] does not match pattern ^\\p{Lu}\\p{Ll}+$"));
+            assertEquals(validationMessages.size(), 13);
         }
     }
 }
